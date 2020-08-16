f48db356e440ac654dec34e72bf60203c3c32743
sunnyl0927 <42189721+sunnyl0927@users.noreply.github.com>
2020-08-16 12:25:21 -0700

    updated with airtable api

--- src/App.tsx
+++ src/App.tsx
@@ -2,71 +2,112 @@ import React, { useEffect, useState } from 'react'
 import CompanyCard from './components/CompanyCard'
 import Grid from '@material-ui/core/Grid'
 
-// function App() {
-//   return <CompanyCard />
-// }
+export interface Company {
+  companyName: string;
+  positions: Position[];
+  applicants: Applicant[];
+  email: string;
+  phone: string;
+  startDate: Date;
+  endDate: Date;
+  application: string;
+  meetings: Meeting[];
+}
 
-// export default App
+export interface Position {
+  name: string;
+  description: string;
+  experience: string;
+  neededBy: Company[];
+}
+
+export interface Applicant {
+  name: string;
+  companies: Company[];
+  resume: File;
+  meetings: Meeting[];
+}
+
+export interface Meeting {
+  meetingID: string;
+  meetingTime: Date;
+  applicant: Applicant;
+  company: Company;
+}
 
 function App(): JSX.Element {
-  const [companiesData, setCompaniesData] = useState<any[]>([]);
+  const [companiesData, setCompaniesData] = useState<Company[]>([]);
 
-  useEffect(() => {
-    // const result = fetch('https://api.airtable.com/v0/appyOq7f3eNJZck84/Companies?api_key=keyCISDxj8vRFA7gY');
-    fetch('https://api.airtable.com/v0/appyOq7f3eNJZck84/Companies?api_key=keyCISDxj8vRFA7gY')
-      // console.log(result);
-      // console.log(JSON.stringify(result));
-      // const json = result.then(result.json());
-      // console.log(json);
-      // console.log(json.records);
-      // setCompaniesData(json.records);
-      .then(res => res.json())
-      .then(res => {
-        console.log(res)
-        //console.log(res.records)
-        setCompaniesData(res.records)
-      })
-      .catch(error => console.log(error))
-  }, []);
+  var Airtable = require('airtable');
+  Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyCISDxj8vRFA7gY'
+  });
+  var base = Airtable.base('appyOq7f3eNJZck84');
+
+  base('Companies').select({
+    view: 'All applicants'
+  }).firstPage(function (err: any, records: any) {
+    if (err) { console.error(err); return; }
+    records.forEach(function (record: any) {
+      setCompaniesData((companiesData) => [
+        ...companiesData,
+        {
+          companyName: record.get('companyName'),
+          positions: record.get('position'),
+          applicants: record.get('applicants'),
+          email: record.get('email'),
+          phone: record.get('phone'),
+          startDate: record.get('startDate'),
+          endDate: record.get('endDate'),
+          application: record.get('application'),
+          meetings: record.get('meetings'),
+        },
+      ]);
+    });
+  });
+
+  base('Companies').select({
+    // Selecting the first 3 records in All applicants:
+    view: "All applicants"
+  }).firstPage(function page(records: any, fetchNextPage: any) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function (record: any) {
+      setCompaniesData((companiesData) => [
+        ...companiesData,
+        {
+          companyName: record.get('companyName'),
+          positions: record.get('position'),
+          applicants: record.get('applicants'),
+          email: record.get('email'),
+          phone: record.get('phone'),
+          startDate: record.get('startDate'),
+          endDate: record.get('endDate'),
+          application: record.get('application'),
+          meetings: record.get('meetings'),
+        },
+      ])
+    });
+
+    console.log(companiesData);
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+  }, function done(err: any) {
+    if (err) { console.error(err); return; }
+  });
 
   return (
     <Grid container direction='row' spacing={2}>
       {companiesData.map(company => (
-        <CompanyCard companiesData={companiesData} />
+        <CompanyCard companyData={company} key={company.companyName} />
       ))}
     </Grid>
   );
 }
-
-
-// class App extends React.Component {
-//   constructor(props: React.Component) {
-//     super(props);
-//     this.state = {
-//       companiesData: any[]
-//     }
-//   }
-
-//   componentDidMount() {
-//     fetch('https://api.airtable.com/v0/appyOq7f3eNJZck84/Companies?api_key=keyCISDxj8vRFA7gY')
-//       .then(res => res.json())
-//       .then(res => {
-//         console.log(res.records)
-//         this.setState({ companiesData: res.records })
-//       })
-//       .catch(error => console.log(error))
-//   }
-
-//   render() {
-//     const { companiesData } = this.state
-//     return (
-//       <Grid container direction='row' spacing={2}>
-//         {companiesData.map(company => (
-//           <CompanyCard {...company.fields} key={company.fields.id} />
-//         ))}
-//       </Grid>
-//     )
-//   }
-// }
 
 export default App
